c381fecf2f90c169736e5b29dd43050bb695eb4b
Dovias <dovio@proton.me>
2024-09-05 00:04:00 +0300

    Add tailwind configuration

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,11 @@
 import {defineConfig} from "astro/config";
 
 import {contentCollectionLocales} from "./src/locale/astro/integration";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   "integrations": [
+    tailwind(),
     contentCollectionLocales({
       "i18n": {
         "defaultLocale": "en",
---
+++ tailwind.config.ts
@@ -0,0 +1,36 @@
+import type {Config} from "tailwindcss";
+
+export default {
+	"content": ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	"theme": {
+		"extend": {
+			"borderWidth": {
+				"2": "0.178rem",
+				"4": "0.2rem"
+			},
+			"fontSize": {		
+				"heading-100": "var(--size-text-heading-100)",
+				"heading-200": "var(--size-text-heading-200)",
+				"heading-300": "var(--size-text-heading-300)",
+
+				"paragraph": "var(--size-text-paragraph)"
+			},
+			"colors": {
+				"primary": "rgba(var(--color-primary))",
+				"secondary": "rgba(var(--color-secondary))",
+				"foreground": "rgba(var(--color-foreground))",
+				"background": "rgba(var(--color-background))",
+				"selection": "rgba(var(--color-selection))",
+				"contour": "rgba(var(--color-contour))"
+			},
+
+			"textColor": {
+				"primary": "rgba(var(--color-text-primary))",
+				"secondary": "rgba(var(--color-text-secondary))",
+				"foreground": "rgba(var(--color-text-foreground))",
+				"background": "rgba(var(--color-text-background))",
+				"selection": "rgba(var(--color-text-selection))"
+			}
+		}
+	}
+} satisfies Config;
