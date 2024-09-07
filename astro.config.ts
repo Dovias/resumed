3a80ced79d210a92bd756f12e53c78d91584acd4
Dovias <dovio@proton.me>
2024-09-07 19:27:10 +0300

    Nitpick: Organize astro configuration imports

--- astro.config.ts
+++ astro.config.ts
@@ -1,17 +1,20 @@
 import {defineConfig} from "astro/config";
 
-import {contentCollectionLocales} from "./src/locale/astro/integration";
 import tailwind from "@astrojs/tailwind";
+
+import {contentCollectionLocales} from "./src/locale/astro/integration";
 
 export default defineConfig({
   "integrations": [
     tailwind(),
     contentCollectionLocales({
-      "defaultLocale": "en",
-      "routing": {
-        "redirectToDefaultLocale": true,
-        "prefixDefaultLocale": true,
-        "fallbackType": "redirect"
+      "i18n": {
+        "defaultLocale": "en",
+        "routing": {
+          "redirectToDefaultLocale": true,
+          "prefixDefaultLocale": true,
+          "fallbackType": "redirect"
+        }
       }
     })
   ]
