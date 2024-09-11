fbc7bd6ef8e13cd685d1ce25ea42416c883032a0
Dovias <dovio@proton.me>
2024-09-11 22:54:58 +0300

    Remove tailwind from the project

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,9 @@
 import {defineConfig} from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
-
 import {contentCollectionLocales} from "./src/locale/astro/integration";
 
 export default defineConfig({
   "integrations": [
-    tailwind(),
     contentCollectionLocales({
       "defaultLocale": "en",
       "routing": {
