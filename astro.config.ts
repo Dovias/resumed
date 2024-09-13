d7181e71f06229e4bf009815469c3bb2a33f2427
Dovias <dovio@proton.me>
2024-09-13 19:27:41 +0300

    Enable order unsafe css bundle optimizations

--- astro.config.ts
+++ astro.config.ts
@@ -2,10 +2,27 @@ import {defineConfig} from "astro/config";
 
 import {contentCollectionLocales} from "./src/locale/astro/integration";
 
-import cssnano from "cssnano";
-
 // @ts-expect-error @csstools/postcss-sass does not have type declarations
 import sass from "@csstools/postcss-sass";
+
+import _postcssMergePlus from "postcss-merge-rules-plus";
+
+// @ts-expect-error @hail2u/css-mqpacker does not have type declarations
+import mediaQueryPacker from "@hail2u/css-mqpacker";
+
+
+import cssnano from "cssnano";
+
+/*
+ * Due to TypeScript's strict handling of ESM-to-CommonJS transpilation,
+ * the `default` export is placed on `module.exports.default` instead of directly on `module.exports`.
+ * This causes issues in modern environments (e.g., Vite) that expect CommonJS exports.
+ * The type declaration is also incorrect, so we need to manually extract the default property
+ * to properly call the plugin creator function.
+ */
+const {"default": postcssMergePlus} = _postcssMergePlus as unknown as {
+  "default": typeof _postcssMergePlus;
+};
 
 export default defineConfig({
   "integrations": [
@@ -32,6 +49,8 @@ export default defineConfig({
       "postcss": {
         "plugins": [
           sass(),
+          mediaQueryPacker(),
+          postcssMergePlus(),
           cssnano()
         ]
       }
